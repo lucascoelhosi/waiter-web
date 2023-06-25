6c3fb85eb23c09f27ea4623de46f7c9554c60820
Lucas Coelho <lucascoelhosi96@gmail.com>
2023-06-25 19:42:37 -0300

    feat: eslint/config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 const App = () => {
 
-  return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<h1>Hello World</h1>
+			</div>
+		</>
+	)
 }
 
 export default App
